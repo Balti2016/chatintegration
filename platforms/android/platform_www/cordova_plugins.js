5bd63e3dcc9139ff820fb42f804612e4612b2f01
S.Balakrishnan <research@kptechsolutions.com>
2018-07-11 14:23:11 +0530

    First commit

---
+++ platforms/android/platform_www/cordova_plugins.js
@@ -0,0 +1,18 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "id": "com.applozic.phonegap.applozic",
+        "file": "plugins/com.applozic.phonegap/www/applozic.js",
+        "pluginId": "com.applozic.phonegap",
+        "clobbers": [
+            "applozic"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "com.applozic.phonegap": "0.8.0"
+};
+// BOTTOM OF METADATA
+});
